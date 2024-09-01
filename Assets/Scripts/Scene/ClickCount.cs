2f52f301137a7309507ec6819656754530f1c413
Jason <jsonchn@163.com>
2024-09-01 13:34:17 +0800

    完成仓库

---
+++ Assets/Scripts/Scene/ClickCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCount : MonoBehaviour
+{
+    private int mCount;
+
+    public GameObject mObj;
+
+    public void OnClick()
+    {
+        mCount++;
+        if (mCount % 2 == 1)
+        {
+            mObj.SetActive(false);
+        }
+        else
+        {
+            mObj.SetActive(true);
+        }
+    }
+}
